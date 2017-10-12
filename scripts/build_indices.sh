16f6596a0fa047b801d9d3144057b469cc02f252
Faegheh Hasibi <hasibi.44@gmail.com>
2017-10-12 18:15:00 +0200

    build_indices script

--- scripts/build_indices.sh
+++ scripts/build_indices.sh
@@ -1,10 +1,15 @@
-# --
-# DBpedia Types index
-echo "############ Building DBpedia types index ..."
-python -m nordlys.core.data.dbpedia.indexer_dbpedia_types data/config/index_dbpedia_2015_10_types.config.json
+if [ $1 = 'dbpedia' ]
+then
+        echo "############ Building DBpedia index ..."
+        python -m nordlys.core.data.dbpedia.indexer_dbpedia data/config/index_dbpedia_2015_10.config.json
 
-# --
-# DBpedia URI-only index
-echo "############ Importing DBpedia URI-only index ..."
-python -m nordlys.core.data.dbpedia.indexer_dbpedia_uri data/config/index_dbpedia_2015_10_uri.config.json
+elif [ $1 == 'types' ]
+then
+        echo "############ Building DBpedia types index ..."
+        python -m nordlys.core.data.dbpedia.indexer_dbpedia_types data/config/index_dbpedia_2015_10_types.config.json
+elif [ $1 == 'dbpedia_uri' ]
+then
+        echo "############ Building DBpedia URI-only index ..."
+        python -m nordlys.core.data.dbpedia.indexer_dbpedia_uri data/config/index_dbpedia_2015_10_uri.config.json
 
+fi
