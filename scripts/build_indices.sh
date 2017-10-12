7f5904895bbb28ad1d942b7c2d1063e442bcb61f
Faegheh Hasibi <hasibi.44@gmail.com>
2017-10-12 18:15:30 +0200

    Update build_indices.sh

--- scripts/build_indices.sh
+++ scripts/build_indices.sh
@@ -2,7 +2,6 @@ if [ $1 = 'dbpedia' ]
 then
         echo "############ Building DBpedia index ..."
         python -m nordlys.core.data.dbpedia.indexer_dbpedia data/config/index_dbpedia_2015_10.config.json
-
 elif [ $1 == 'types' ]
 then
         echo "############ Building DBpedia types index ..."
@@ -11,5 +10,4 @@ elif [ $1 == 'dbpedia_uri' ]
 then
         echo "############ Building DBpedia URI-only index ..."
         python -m nordlys.core.data.dbpedia.indexer_dbpedia_uri data/config/index_dbpedia_2015_10_uri.config.json
-
 fi
