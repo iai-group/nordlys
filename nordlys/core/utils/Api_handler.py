0ed8e95b90173de9b4984982dc1fd335b470e3e9
Fadwa-Maatug <46538884+Fadwa-Maatug@users.noreply.github.com>
2020-11-03 13:09:58 +0100

    fix typos

--- nordlys/core/utils/Api_handler.py
+++ nordlys/core/utils/Api_handler.py
@@ -10,9 +10,9 @@ class API_Handler(object):
     """Handler for api keys"""
 
     def __init__(self, Api_Log_Path, limit):
-        self.limit_qouta = limit
-        self.api_path = "{0}/api_keys.json".format(Api_Log_Path)  # holds API key-qouta file path
-        with open(self.api_path) as json_file:  # loading the API key-qouta file
+        self.limit_quota = limit
+        self.api_path = "{0}/api_keys.json".format(Api_Log_Path)  # holds API key-quota file path
+        with open(self.api_path) as json_file:  # loading the API key-quota file
             self.api_file = json.load(json_file)
         with open("config/api.json") as json_file:  # loading the API config file
             self.host_file = json.load(json_file)
@@ -22,14 +22,14 @@ class API_Handler(object):
             return "The IP_address is not authorized."
         #       elif api_key is None:
         #           return "Api key is not specified."
-        elif api_key not in self.api_file.keys():  # check if the key is registered in API key-qouta file
+        elif api_key not in self.api_file.keys():  # check if the key is registered in API key-quota file
             return "API key is not authorized."
         elif self.api_file.get(api_key).get(
-                'qouta') >= self.limit_qouta:  # check if the specific api key quota is not exceeded
+                'quota') >= self.limit_quota:  # check if the specific api key quota is not exceeded
             return "API quota exceeded."
         else:
             return True
 
-    def update_api_file(self):  # update the API key-qouta file
+    def update_api_file(self):  # update the API key-quota file
         with open(self.api_path) as json_file:
             self.api_file = json.load(json_file)
